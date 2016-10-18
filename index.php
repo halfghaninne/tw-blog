3d79d4472b17151730bae3d0835f089e127f58fc
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-10-18 16:10:40 -0500

    submitting for design audit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<div>
+			<img src="./public/tw-logo.png" class="logo">
+		</div>
+<!-- 		<div class="page-navigation"> -->
+			<ul class="page-navigation">
+				<li class="page-navigation__link">
+					<a href="" class="nav-link">Articles</a>
+				</li>
+				<li class="page-navigation__link">
+					<a href="" class="nav-link">Notes</a>
+				</li>
+				<li class="page-navigation__link">
+					<a href="" class="nav-link">Info</a>
+				</li> 
+				<li class="page-navigation__link">
+					<a href="" class="nav-link">Search</a>
+				</li>
+			</ul>
+<!-- 		</div> -->
+	</div>
+
+	<div class="intro">
+		<h1 class="intro__paragraph">
+			I'm a designer and web builder who works with two of my best friends at <a href="#" class="link">Paravel</a>.
+		</h1>
+		<div class="intro__social-links">
+			<a href="" class="link">Twitter</a> / 
+			<a href="" class="link">Dribbble</a> / 
+			<a href="" class="link">Codepen</a> / 
+			<a href="" class="link">GitHub</a> / 
+			<a href="" class="link">Letterboxd</a> / 
+			<a href="" class="link">DayTrip</a>
+		</div>
+	</div>
+
+	<div class="work">
+	
+		<div class="work-item">
+			<img class="work-item__thumbnail" src="./public/daytrip.jpg">
+			<p class="work-item__description">
+				Near and dear to our hearts, DayTrip is a Paravel project designed to make it easy to find something great to do and someplace great to go. Austin/Texas area private beta out now.
+			</p>
+		</div>
+
+		<div class="more-work">
+			More: 
+			<a href="" class="link">Info</a>, 
+			<a href="" class="link">Articles</a>, or 
+			<a href="" class="link">Notes</a>	
+		</div>
+	</div>
+
+	<div class="footer">
+		<div class="footer__items">
+			<div>
+				Follow me on Twitter
+			</div>
+			<div class="footer__items footer__items-left">
+				<div>
+					Hire Paravel
+				</div>
+				<div>
+					RSS Feed
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
